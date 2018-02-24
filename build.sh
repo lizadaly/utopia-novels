4a4290f2466624acb4d491abc114742152cd0e72
Liza Daly <lizadaly@gmail.com>
2018-02-23 19:35:43 -0500

    Add link to the Medium piece and instructions on PRs

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ cat > $readme <<END
 # 19th century utopian novels by women
 A small collection of 19th/20th century utopian fiction.
 
-This collection was part of the research material for the interactive fiction story, [Harmonia](https://github.com/lizadaly/harmonia).
+This collection was part of the research material for the interactive fiction story, [Harmonia](https://github.com/lizadaly/harmonia) and is the subject of an article on [early women's utopian fiction](https://medium.com/@liza/her-stories-fictional-utopias-of-19th-century-women-247c457c4f16).
 
-Contributions, corrections, and proofed new editions are very welcome; please issue a <a href="https://opensource.guide/how-to-contribute/">pull request</a>. 
+Contributions, corrections, and proofed new editions are very welcome; please issue a <a href="https://opensource.guide/how-to-contribute/">pull request</a> against one of the Markdown files.
 
 The current collection:
 END
